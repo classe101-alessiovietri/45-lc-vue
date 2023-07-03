81eb374da506f5da4276de47cfd0b074a4314359
alessiovietri <info@alessiovietri.it>
2023-07-03 10:42:30 +0200

    Completato recap

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,29 @@
-console.log('Template HTML CSS JS');
+// Verifico che l'oggettone Vue sia stato importato
+console.log(Vue);
+
+// Estraggo il metodo createApp dall'oggettone Vue (tramite destructuring)
+const { createApp } = Vue;
+
+// Verifico di aver estratto correttamente il metodo createApp
+console.log(createApp);
+
+createApp({
+    data() {
+        return {
+            name: 'Classe 101',
+            studentsNumber: 35,
+            bon: {
+                firstName: 'Francesco',
+                lastName: 'Bonandin',
+            }
+        };
+    },
+    methods: {
+        gestisciStoClick() {
+            alert('Cliccato bottone');
+        },
+        sopraAlBottone() {
+            console.log(this.bon);
+        }
+    }
+}).mount('#app');
